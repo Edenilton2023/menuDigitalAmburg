8de5e3a3315527f41186126f41d0820fa9153d1b
Edenilton <142240436+Edenilton2023@users.noreply.github.com>
2024-08-23 02:28:20 -0300

    abre e fecha modal

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-const menu = document.getElementById('menu');
-const cartBtn = document.getElementById('cart-btn');
 
-console.log(cartBtn);
 
-// parei 12:00 / 1:34:03
+
+
+
+
+
+const menu = document.querySelector('#menu');
+const cartBtn = document.querySelector('#cart-btn');
+const cartModal =document.querySelector('#cart-modal')
+const cartItemsContainer = document.querySelector('#cart-items');
+const cartTotal = document.querySelector('#cart-total');
+const checkoutBtn = document.querySelector('#checkout-btn');
+const closeModalBtn = document.querySelector('#close-modal-btn');
+const cartCount = document.querySelector('#cart-count');
+const mesaInput = document.querySelector('#mesa');
+const codCart = document.querySelector('#cod-cart');
+const addresWarn = document.querySelector('#dados-errado');
+
+
+cartBtn.addEventListener('click',()=>{
+ cartModal.style.display = ' flex';
+})
+closeModalBtn.addEventListener('click',()=>{
+  cartModal.style.display = 'none';
+})
+
+
